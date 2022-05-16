410fe9b2e78a2325391a5e7dc8a9a73dd4465527
Anshul <aguptdiff@gmail.com>
2022-05-16 19:33:24 +0530

    worked

--- room_slot_booking/urls.py
+++ room_slot_booking/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('hotel/', include('hotel.urls', namespace='hotel')),
-#    path('',views.home,name="home"),
-    path('hotel/',include ('django.contrib.auth.urls')),
-    path('home/', include('hotel.urls')),
+    path('hotel/',include('hotel.urls')),
 ]
