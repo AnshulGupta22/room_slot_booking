e77678c6382581fec8021d069b376ea565082d59
Anshul <aguptdiff@gmail.com>
2022-05-21 19:34:31 +0530

    done

--- hotel/models.py
+++ hotel/models.py
@@ -7,4 +7,4 @@ class Hotel(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     email = models.EmailField()
-    #password = models.CharField(max_length=120)
+
