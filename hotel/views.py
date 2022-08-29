a3ddce7fc59842c2120ffe1b692110c9ba5af34a
Anshul <aguptdiff@gmail.com>
2022-08-30 00:01:19 +0530

    before css

--- hotel/views.py
+++ hotel/views.py
@@ -9,12 +9,14 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import (api_view, permission_classes)
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+
 from .serializers import (RoomSerializer, CustomerSerializer,
                             BookingSerializerBook,  BookingSerializerAdmin,
                             BookingSerializerGet,
                             BookingSerializerAdminWithoutid,
                             CustomerAPISerializer)
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+
 
 from django.contrib.auth.models import User
 from hotel.forms import CustomerForm, SignInForm, BookingForm
